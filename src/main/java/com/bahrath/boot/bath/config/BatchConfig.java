9d39c0d1aaf04ecc659a6d66b69e922869164076
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2021-11-17 15:42:11 -0500

    Configure the step

--- src/main/java/com/bahrath/boot/bath/config/BatchConfig.java
+++ src/main/java/com/bahrath/boot/bath/config/BatchConfig.java
@@ -4,11 +4,27 @@ import com.bahrath.boot.bath.MyJobListener;
 import com.bahrath.boot.bath.Processor;
 import com.bahrath.boot.bath.Reader;
 import com.bahrath.boot.bath.Writer;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BatchConfig {
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Step step() {
+        return stepBuilderFactory.get("step1")
+                .<String, String>chunk(1)
+                .reader(reader())
+                .processor(processor())
+                .writer(writer())
+                .build();
+    }
 
     @Bean
     public Reader reader() {
