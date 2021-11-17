77bfd230eaffc44c2f3eeaf036af5e5f58abbe10
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2021-11-17 15:09:02 -0500

    Create a Reader

---
+++ src/main/java/com/bahrath/boot/bath/Reader.java
@@ -0,0 +1,22 @@
+package com.bahrath.boot.bath;
+
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
+
+public class Reader implements ItemReader<String> {
+
+    private String[] courses = {"Java web Services", "End to End Project", "Angular"};
+    private int count;
+
+    @Override
+    public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+        if(count<courses.length) {
+            return courses[count++];
+        } else {
+            count = 0;
+        }
+        return null;
+    }
+}
