0c241e7b241bc86d0723c06c0c4e55003363701d
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2021-11-17 18:08:12 -0500

    Writint a test

--- src/test/java/com/bahrath/boot/bath/SpringbatchApplicationTests.java
+++ src/test/java/com/bahrath/boot/bath/SpringbatchApplicationTests.java
@@ -1,13 +1,30 @@
 package com.bahrath.boot.bath;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbatchApplicationTests {
 
+	@Autowired
+	JobLauncher jobLauncher;
+
+	@Autowired
+	private Job job;
+
 	@Test
-	void contextLoads() {
+	public void testBatch() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+		JobParameters jobParameters = new JobParametersBuilder().addLong("time", System.currentTimeMillis()).toJobParameters();
+		jobLauncher.run(job, jobParameters);
 	}
 
 }
