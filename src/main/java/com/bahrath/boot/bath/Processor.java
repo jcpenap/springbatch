dbd61885b265d2d00a341574cd363ac109b2360c
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2021-11-17 15:17:37 -0500

    Adding Processor

---
+++ src/main/java/com/bahrath/boot/bath/Processor.java
@@ -0,0 +1,11 @@
+package com.bahrath.boot.bath;
+
+import org.springframework.batch.item.ItemProcessor;
+
+public class Processor implements ItemProcessor<String, String> {
+    @Override
+    public String process(String item) throws Exception {
+        System.out.println("Inside Process");
+        return "PROCESSED "+item.toUpperCase();
+    }
+}
