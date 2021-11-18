5bd80152ecc89cac913819cb2a36f04659c80f1f
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2021-11-18 15:38:19 -0500

    Executing first Batch

--- src/main/java/com/bahrath/boot/bath/Reader.java
+++ src/main/java/com/bahrath/boot/bath/Reader.java
@@ -12,6 +12,7 @@ public class Reader implements ItemReader<String> {
 
     @Override
     public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+        System.out.println("Inside read method");
         if(count<courses.length) {
             return courses[count++];
         } else {
